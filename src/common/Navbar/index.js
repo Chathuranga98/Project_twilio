84fb95953baac723bc80b7cba2bfe531ec6ded10
Chathuranga98 <2019cs018@stu.ucsc.lk>
2022-08-31 07:49:58 +0530

    this commit for nav bar

---
+++ src/common/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css';
+
+function Navbar(){
+    return(
+
+        <h1 className="title is-1">navbar</h1> 
+
+    );
+}
+
+export default Navbar;
--- src/screens/Aboutpage/index.js
+++ src/screens/Aboutpage/index.js
@@ -4,7 +4,7 @@ import './index.css';
 function Aboutpage(){
     return(
 
-        <h1>This is about page</h1> 
+        <h1 className="title is-1">This is about page</h1> 
 
     );
 }
